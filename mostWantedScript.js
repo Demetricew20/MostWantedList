d5a9f0a691e0dee82d880e28260e80ebea42dac1
DemetriceW20 <demetrice_williams@hotmail.com>
2021-03-12 07:31:21 -0900

    Deleted placeholders for table data in html file

--- mostWantedScript.js
+++ mostWantedScript.js
@@ -1,5 +1,4 @@
 import {people} from "./data.js";
-import {searchByName} from "./app.js";
 'use strict';
 
 // Table rows for table data begins at tr[1] ///
@@ -13,32 +12,12 @@ function populateData(row) {
 };
 
 function displayData() {
-    populateData(0);
-    populateData(1);
-    populateData(2);
-    populateData(3);
-    populateData(4);
-    populateData(5);
-    populateData(6);
-    populateData(7);
-    populateData(8);
-    populateData(9);
-    populateData(10);
-    populateData(11);
-    populateData(12);
-    populateData(13);
-    populateData(14);
-    populateData(15);
-    populateData(16);
-    populateData(17);
-    populateData(18);
-    populateData(19);
-    populateData(20);
-    populateData(21);
+    for (let i = 0; i < 22; i++){
+        populateData(i);
+    }
 };
 
 displayData();
-
 
 
 

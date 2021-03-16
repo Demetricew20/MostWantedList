9aefd8537512f1c3fcb9a1fe775b6d93258a9243
DemetriceW20 <demetrice_williams@hotmail.com>
2021-03-16 13:18:20 -0800

    Add more comments for clarity.

--- galleryScript.js
+++ galleryScript.js
@@ -7,11 +7,13 @@ function galleryData()
     let cardTitle = document.getElementsByClassName('card-title');
     let cardBody = document.getElementsByClassName('card-text');
     let cardImage = document.getElementsByTagName('img');
+    // Placeholder images if photo is not found //
     let defaultImg = "images\\no-img.png";
 
 
     for (let i = 0; i < people.length; i++)
     {
+        // Adds Name, Occupation, and Photo to gallery //
         cardTitle[i].innerHTML = `${people[i].firstName} ${people[i].lastName}`;
         cardBody[i].innerHTML = people[i].occupation.toUpperCase();
         let photoHtml = people[i].photo;

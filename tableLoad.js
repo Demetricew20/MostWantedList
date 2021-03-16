00b3ccd3b64988e5ecd4a13b4984ba4d138bb36d
DemetriceW20 <demetrice_williams@hotmail.com>
2021-03-15 18:47:59 -0800

    Fixed responsive nav bar for gallery page. Still need to work on home page nav bar responsiveness

--- tableLoad.js
+++ tableLoad.js
@@ -1,52 +1,52 @@
-// Helper Function: Build family row
-function buildRow(rowTypeString, id, currRow, tableBody) {
-    let tempRow = rowTypeString;
-    let tempPerson = rtvPeopleByID(id);
-    tempRow = tempRow + tempPerson[0] + ' ' + tempPerson[1];
-    let row = tableBody.insertRow(currRow);
-    row.insertCell(1)
-    row.insertCell(2)
-    document.getElementsByTagName('tr')[currRow].getElementsByTagName('td')[1].innerHTML = tempRow
-}
+// // Helper Function: Build family row
+// function buildRow(rowTypeString, id, currRow, tableBody) {
+//     let tempRow = rowTypeString;
+//     let tempPerson = rtvPeopleByID(id);
+//     tempRow = tempRow + tempPerson[0] + ' ' + tempPerson[1];
+//     let row = tableBody.insertRow(currRow);
+//     row.insertCell(1)
+//     row.insertCell(2)
+//     document.getElementsByTagName('tr')[currRow].getElementsByTagName('td')[1].innerHTML = tempRow
+// }
 
-function loadTable(someArray) {
-    let currentRow = 0
-    let tableBody = document.getElementById('tableBody');
-    let tableHeaders = document.getElementsByTagName('th');
+// function loadTable(someArray) {
+//     let currentRow = 0
+//     let tableBody = document.getElementById('tableBody');
+//     let tableHeaders = document.getElementsByTagName('th');
 
-    for (let i = 0; i < someArray.length; i++) {
-        // add row
-        let row = tableBody.insertRow(currentRow)
-        // build cells
-        for (let j = 0; j < tableHeaders.length; j++) {
-            row.insertCell(j)
-        }
-        console.log("tr:"+tr+" td:"+td+" i:"+i)
-        document.getElementsByTagName('tr')[currentRow].getElementsByTagName('td')[0].innerHTML = someArray[i].firstName;
-        console.log("td: "+td+" element: ",someArray[i].firstName )
-        document.getElementsByTagName('tr')[currentRow].getElementsByTagName('td')[1].innerHTML = someArray[i].lastName;
-        document.getElementsByTagName('tr')[currentRow].getElementsByTagName('td')[2].innerHTML = someArray[i].dob;
-        document.getElementsByTagName('tr')[currentRow].getElementsByTagName('td')[3].innerHTML = someArray[i].gender;
-        document.getElementsByTagName('tr')[currentRow].getElementsByTagName('td')[4].innerHTML = someArray[i].eyeColor;
-        document.getElementsByTagName('tr')[currentRow].getElementsByTagName('td')[5].innerHTML = someArray[i].occupation;
-        // Add photo button here
+//     for (let i = 0; i < someArray.length; i++) {
+//         // add row
+//         let row = tableBody.insertRow(currentRow)
+//         // build cells
+//         for (let j = 0; j < tableHeaders.length; j++) {
+//             row.insertCell(j)
+//         }
+//         console.log("tr:"+tr+" td:"+td+" i:"+i)
+//         document.getElementsByTagName('tr')[currentRow].getElementsByTagName('td')[0].innerHTML = someArray[i].firstName;
+//         console.log("td: "+td+" element: ",someArray[i].firstName )
+//         document.getElementsByTagName('tr')[currentRow].getElementsByTagName('td')[1].innerHTML = someArray[i].lastName;
+//         document.getElementsByTagName('tr')[currentRow].getElementsByTagName('td')[2].innerHTML = someArray[i].dob;
+//         document.getElementsByTagName('tr')[currentRow].getElementsByTagName('td')[3].innerHTML = someArray[i].gender;
+//         document.getElementsByTagName('tr')[currentRow].getElementsByTagName('td')[4].innerHTML = someArray[i].eyeColor;
+//         document.getElementsByTagName('tr')[currentRow].getElementsByTagName('td')[5].innerHTML = someArray[i].occupation;
+//         // Add photo button here
 
-        // Add Spouse
-        if (someArray[i].spouse !== "")
-        {
-            currentRow++;
-            buildRow("Spouse: ",someArray[i].spouse,currentRow,tableBody )
-        }
+//         // Add Spouse
+//         if (someArray[i].spouse !== "")
+//         {
+//             currentRow++;
+//             buildRow("Spouse: ",someArray[i].spouse,currentRow,tableBody )
+//         }
 
-        // Add Parents
-        if (someArray[i].parents !== "") {
-            for (let j = 0; j < someArray[i].parents.length; j++) {
-                currentRow++;
-                buildRow("Parent: ", someArray[i].parents[j],currentRow,tableBody)
-            }
-        }
+//         // Add Parents
+//         if (someArray[i].parents !== "") {
+//             for (let j = 0; j < someArray[i].parents.length; j++) {
+//                 currentRow++;
+//                 buildRow("Parent: ", someArray[i].parents[j],currentRow,tableBody)
+//             }
+//         }
 
-        currentRow++;
-    }
+//         currentRow++;
+//     }
 
-}
+// }

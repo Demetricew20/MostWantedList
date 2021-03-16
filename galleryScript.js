6dc3f0b2aa561a7480344a20eb5464e779c4300c
DemetriceW20 <demetrice_williams@hotmail.com>
2021-03-16 12:30:20 -0800

    Removed hard code from gallery.html. Add media breakpoints for scaling of images.

--- galleryScript.js
+++ galleryScript.js
@@ -7,23 +7,27 @@ function galleryData()
     let cardTitle = document.getElementsByClassName('card-title');
     let cardBody = document.getElementsByClassName('card-text');
     let cardImage = document.getElementsByTagName('img');
+    let defaultImg = "images\\no-img.png";
 
 
     for (let i = 0; i < people.length; i++)
     {
         cardTitle[i].innerHTML = `${people[i].firstName} ${people[i].lastName}`;
-        cardBody[i].innerHTML = people[i].occupation;
+        cardBody[i].innerHTML = people[i].occupation.toUpperCase();
         let photoHtml = people[i].photo;
-        let defaultImg = "images\\no-img.png"
         if (people[i].photo === undefined)
         {
             cardImage[i].src = defaultImg;
         }
         else{
+
             cardImage[i].src = "images\\" + photoHtml;
+
         }
     }
 
 }
+
+
 
 galleryData();

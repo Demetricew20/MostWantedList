7158625bfc1211e3bce856f6a68344a963466c9c
cashmy <cmyers880@gmail.com>
2021-03-15 21:28:57 -0500

    Cleaned up file mostWantedScript.js of old code (refactor)

--- mostWantedScript.js
+++ mostWantedScript.js
@@ -1,59 +1,5 @@
 'use strict';
 
-// function populateData(row)
-// {
-
-//     document.getElementsByTagName('tr')[row+1].getElementsByTagName('td')[0].innerHTML = people[row].id;
-//     document.getElementsByTagName('tr')[row+1].getElementsByTagName('td')[1].innerHTML = people[row].firstName;
-//     document.getElementsByTagName('tr')[row+1].getElementsByTagName('td')[2].innerHTML = people[row].lastName;
-//     document.getElementsByTagName('tr')[row+1].getElementsByTagName('td')[3].innerHTML = people[row].dob;
-//     document.getElementsByTagName('tr')[row+1].getElementsByTagName('td')[4].innerHTML = people[row].gender;
-//     document.getElementsByTagName('tr')[row+1].getElementsByTagName('td')[5].innerHTML = people[row].eyeColor;
-//     document.getElementsByTagName('tr')[row+1].getElementsByTagName('td')[6].innerHTML = people[row].occupation;
-
-//     // ****** Add photos for each person in table ****** ///
-//     let photos = document.getElementsByTagName('tr')[row+1].getElementsByTagName('td')[7];
-//     photos.innerHTML = '<button id="photo">Photo</button>';
-//     photos.addEventListener('click', function(){
-
-//         if (photos.innerHTML == '<button id="photo">Photo</button>')
-//         {
-//             photos.innerHTML = '<img id="photo" src="https://picsum.photos/100/100" alt="Random Photo" />';
-//         }
-//         else
-//         {
-//             photos.innerHTML = '<button id="photo">Photo</button>';
-//         }
-//     });
-
-// };
-
-// // ----Adds td and tr tags in the body of mostWantedTable---- //
-// function addElement() {
-
-//     let tableBody = document.getElementById('tableBody');
-//     let tableHeaders = document.getElementsByTagName('th');
-
-//     // Create Rows using array of people length //
-//     for (let i = 0; i < people.length; i++) {
-//         let row = tableBody.insertRow(0);
-//         // Create cells for columns of criteria (first,last,....etc)
-//         for (let i = 0; i < tableHeaders.length; i++) {
-//             row.insertCell(i);
-//         };
-//     };
-
-//     function insertData() {
-//         for (let i = 0; i < people.length; i++) {
-//             populateData(i)
-//         }
-//     };
-
-//     insertData();
-
-// };
-
-
 // -------
 // Helper Function: Build family row
 function buildRow(rowTypeString, id, currRow, tableBody) {
@@ -69,6 +15,7 @@ function buildRow(rowTypeString, id, currRow, tableBody) {
     // document.getElementsByTagName('tr')[currRow].getElementsByTagName('td')[1].innerHTML = tempRow
 }
 
+// ----- Load Table with incoming data as a parameter -------
 function loadTable(someArray) {
     let currentRow = 0;
     let tableBody = document.getElementById('tableBody');
@@ -112,10 +59,8 @@ function loadTable(someArray) {
 
     }
 
+// ------ Clear Table rows for reload of data (used after filter has been applied)
 function deleteTable() {
-    // let delTable = document.getElementById("mostWantedTable");
-    // delTable.innerHTML = "";
-
     let tableHeaderRowCount = 1;
     let table = document.getElementById("mostWantedTable");
     let rowCount = table.rows.length;
@@ -126,8 +71,4 @@ function deleteTable() {
 
 
 // Function Calls //
-// testDataRtv(); // Testing Routing only
-// window.onload = addElement();
-window.onload = loadTable(people);
-// insertData();
-
+window.onload = loadTable(people)
